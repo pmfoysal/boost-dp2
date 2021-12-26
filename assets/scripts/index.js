5e9449a2fe1a7d92119d8b4ae89c6dbfc7de92cb
pmfoysal <pmfoysal@gmail.com>
2021-12-26 13:14:43 +0600

    Color Issues Fixed

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,11 +1,17 @@
 document.querySelector('button').addEventListener('click', function () {
-   document.body.style.backgroundColor = generateColor();
-   document.querySelector('h2').innerHTML = generateColor();
+   const color = generateColor();
+   document.body.style.backgroundColor = color;
+   document.querySelector('h2').innerHTML = color;
 });
 
 function generateColor() {
-   const red = Math.floor(Math.random() * 255);
-   const green = Math.floor(Math.random() * 255);
-   const blue = Math.floor(Math.random() * 255);
-   return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+   let red = Math.floor(Math.random() * 255).toString(16);
+   let green = Math.floor(Math.random() * 255).toString(16);
+   let blue = Math.floor(Math.random() * 255).toString(16);
+
+   red = red.length < 2 ? `0${red}` : red;
+   green = green.length < 2 ? `0${green}` : green;
+   blue = blue.length < 2 ? `0${blue}` : blue;
+
+   return `#${red}${green}${blue}`;
 }
